57442184a2dd1c4eaa1ab781219df61b404310c4
Jose <josekasike@gmail.com>
2019-12-17 14:43:47 -0400

    inicio del servidor y hola mundo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function presentacion(nombre, mensaje){
+  let mostrar = (
+    <div>
+      <p><strong>{nombre}</strong> {mensaje}</p>
+    </div>
+  )
+    return mostrar
+}
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {presentacion('Jose Del Corral', 'Esta aprendiendo REACT')}
         </p>
         <a
           className="App-link"
